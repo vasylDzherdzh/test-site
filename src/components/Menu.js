c35f256fc8e8badf4d96f996175b3e0feef48b8b
Vasyl Dzhierdzh <vasya.dzhierdzh@gmail.com>
2023-02-24 21:36:09 +0200

    добавлення простого стилю для меню

---
+++ src/components/Menu.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Menu.scss'
+
+const Menu = () => {
+  return (
+    <nav>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Menu;
