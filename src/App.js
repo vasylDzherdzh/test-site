b4e2bfd92165512527c73b60400e4cc875b6840c
Vasyl Dzhierdzh <vasya.dzhierdzh@gmail.com>
2023-02-24 20:22:26 +0200

    добавення масиву із значенням і кнопки видалити

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/shared/Header/Header';
+import Productlist from './components/Productlist/Productlist';
 
 function App() {
+  
+  const [products, setProducts] = useState([
+    {id: 1, title: 'Product 1', price: 899},
+    {id: 2, title: 'Product 2', price: 982},
+    {id: 3, title: 'Product 3', price: 322},
+    {id: 4, title: 'Product 4', price: 763},
+    {id: 5, title: 'Product 5', price: 389}
+  ]);
+
+  const deleteProduct = (productId) => {
+    const newProduct = products.filter(product => product.id !== productId);
+    setProducts(newProduct);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Header/>
+        <Productlist products={products} deleteProduct={ deleteProduct } />
+
     </div>
   );
 }
